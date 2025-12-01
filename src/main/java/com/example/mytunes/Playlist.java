b0612c7fe6cf8599bdce45af8fb6f7cb4fad1c6e
emmRas01 <emmras01@easv365.dk>
2025-12-01 11:30:05 +0100

    Add konstruktør and getter and setter methods

--- src/main/java/com/example/mytunes/Playlist.java
+++ src/main/java/com/example/mytunes/Playlist.java
@@ -1,6 +1,7 @@
 package com.example.mytunes;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Playlist implements Serializable //denne klasse skal kunne gemmes på en fil -> derfor skal man implementere Serializable
 {
@@ -8,8 +9,60 @@ public class Playlist implements Serializable //denne klasse skal kunne gemmes p
 
     //Definition af Attributterne name, songs og time
     private String name;
-    private String songs;
+    private String song;
     private String time;
 
+    //Array-liste til at gemme en eller flere vare-objekter i en ordre
+    private ArrayList<Song> songs = new ArrayList<>();
 
+    //konstruktør
+    public Playlist(String name, String song, String time)
+    {
+        this.name = name;
+        this.song = song;
+        this.time = time;
+    }
+
+    //Metode til at tilføje en sang til arrayListen der indeholder de sange der tilhører en playliste
+    public void tilføjVare(Song s)
+    {
+        songs.add(s);
+    }
+
+    //Metode der returnere hele listen med sange der tilhøre en playliste
+    public ArrayList<Song> getSongsList()
+    {
+        return songs;
+    }
+
+    //Der skal være get-metoder og set-metoder for at tableview Playlist både kan hente data og indsætte ny data
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getSongs()
+    {
+        return song;
+    }
+
+    public String getTime()
+    {
+        return time;
+    }
+
+    public void setName(String n)
+    {
+        name = n;
+    }
+
+    public void setSongs(String s)
+    {
+        song = s;
+    }
+
+    public void setTime(String t)
+    {
+        time = t;
+    }
 }
