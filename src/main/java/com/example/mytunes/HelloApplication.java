be5cd79fd97e1b0bc176dced3593ef5fdcb24889
emmRas01 <emmras01@easv365.dk>
2025-12-03 13:43:04 +0100

    Adjust GUI

--- src/main/java/com/example/mytunes/HelloApplication.java
+++ src/main/java/com/example/mytunes/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MyTunesGUI.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1400, 900);
+        Scene scene = new Scene(fxmlLoader.load(), 1400, 850);
         stage.setTitle("MyTunes");
         stage.setScene(scene);
         stage.show();
